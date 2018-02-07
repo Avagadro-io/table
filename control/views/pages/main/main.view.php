adf0f225fca995ff113e5eae3372641f0fb6381a
Nurmuhammet Allanov <jackutruth@gmail.com>
2018-02-07 21:00:22 +0500

    Testing Functions

---
+++ control/views/pages/main/main.view.php
@@ -0,0 +1,7 @@
+<?php require_once 'views/partials/header.view.php'; ?>
+<!-- Page Content -->
+	<div class="container">
+		<h1>Hello World</h1>
+	</div>	
+<!-- /Page Content -->
+<?php require_once 'views/partials/footer.view.php'; ?>
