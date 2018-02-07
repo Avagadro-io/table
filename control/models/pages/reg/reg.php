99c208966a2836ef8048871b6498667b0f0280ab
Nurmuhammet Allanov <jackutruth@gmail.com>
2018-02-07 23:58:07 +0500

    Added feature add new student

--- control/models/pages/reg/reg.php
+++ control/models/pages/reg/reg.php
@@ -1,7 +1,13 @@
 <?php 
 
-if ($_SERVER['REQUEST_METHOD']) {
-	# code...
+$posts = get_my("users", $conn);
+$data = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$fullName = $_POST['fullName'];
+	query("INSERT INTO users(fullName) VALUES(:fullName)", ['fullName' => $fullName], $conn);
+	$data['message'] = 'Üstünlik';
+	if ($data) extract($data);
 }
 
 require 'views/pages/reg/reg.view.php';
--- control/views/pages/reg/reg.view.php
+++ control/views/pages/reg/reg.view.php
@@ -4,7 +4,7 @@
 body{ font-family: "Ubuntu",sans-serif; }
 .inner-content{
 	width: 65%;
-	margin: auto;
+	margin: 2em auto;
 	padding: 10px 12px;
 	color: #333333;
 	background-color: #eee;
@@ -14,7 +14,26 @@ body{ font-family: "Ubuntu",sans-serif; }
 </style>
 <div class="inner-content">
 	<div style="height: 45px;"></div>
-	
+	<form action="" method="POST">
+		<label for="fullName">Full Name: </label>
+		<input type="text" name="fullName" id="fullName" placeholder="Full Name" class="form-control" required="required">
+		<input type="submit" name="submit" class="btn btn-primary" style="margin-top: 20px; float: right;" value="Goş">
+	</form>
+	<?php if (isset($message)): ?>
+		<div class="alert alert-success alert-dismissible fade in" role="alert" id="alerted-message">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
+            </button>
+            <strong><?= $message ?>!</strong>
+          </div>
+          <script>
+			var likeModal = document.getElementById('alerted-message');
+			likeModal.addEventListener('click', () => {
+					setTimeout( () => { 
+						window.location.replace("/control/"); 
+					}, 850);
+				});
+		</script>
+	<?php endif ?>
 </div>
 <!-- /Page Content -->
 <?php require_once 'views/partials/footer.view.php'; ?>
